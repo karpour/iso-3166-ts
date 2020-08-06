60fd35eafe11f48498a9d94e95fb1af2afc5fcac
Thomas Novotny <thomas@chillhop.com>
2020-08-06 19:00:18 +0200

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+/** ISO3166 Alpha 2 country code -> country name*/
+export declare const ISO_3166_ALPHA_2: {
+    [key: string]: string;
+};
+/** ISO3166 Alpha 2 country code */
+export declare type Iso3166Alpha2Code = keyof typeof ISO_3166_ALPHA_2;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# ISO 3166 Typescript types
+
+Typescript data type for ISO3166-alpha2 country codes, as well as mappings for country code => country name
